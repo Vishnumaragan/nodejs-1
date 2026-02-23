ba34140d3704dcfcd20a5f18aa8a52bf7d93a1b6
nairx <nairxprv@gmail.com>
2026-02-23 10:58:03 +0530

    updated

--- index12.js
+++ index12.js
@@ -1,21 +1,9 @@
 //router
 import express from "express"
+import userRouter from "./routes/userRoutes.js"
+import productRouter from "./routes/productRoutes.js"
 const app = express()
 app.listen(8080,()=>console.log("Server started"))
-const userRouter = express.Router()
-const productRouter = express.Router()
-userRouter.get("/",(req,res)=>{
-res.send("This is get request of user router")
-})
-userRouter.post("/",(req,res)=>{
-res.send("This is post request of user router")
-})
-productRouter.get("/",(req,res)=>{
-res.send("This is get request of product router")
-})
-productRouter.post("/",(req,res)=>{
-res.send("This is post request of product router")
-})
 app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
 
