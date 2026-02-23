43f3b08c71521c4f96e1a1f98e6263c3cc4f5f37
nairx <nairxprv@gmail.com>
2026-02-23 14:42:23 +0530

    updated

--- index16.js
+++ index16.js
@@ -7,19 +7,32 @@ const app = express();
 app.listen(8080, () => {
   console.log("server is starting");
 });
+
 const middleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader)
     return res.status(401).json({ message: "Authorization header missing" });
   const token = authHeader.split(" ")[1];
   try {
-    jwt.verify(token, secretKey);
+    const user = jwt.verify(token, secretKey);
     console.log("middleware executed");
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
   }
 };
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (roles.includes(req.user.role)) {
+      next();
+    } else {
+      res.status(403).json({ message: "Access denied" });
+    }
+  };
+};
+
 app.use(express.json());
 const users = [];
 app.post("/signup", (req, res) => {
@@ -45,6 +58,6 @@ app.post("/login", async (req, res) => {
 app.get("/", middleware, (req, res) => {
   res.json({ message: "welcome to home page" });
 });
-app.get("/users", (req, res) => {
+app.get("/users", middleware, authorize("admin", "manager"), (req, res) => {
   res.json(users);
 });
